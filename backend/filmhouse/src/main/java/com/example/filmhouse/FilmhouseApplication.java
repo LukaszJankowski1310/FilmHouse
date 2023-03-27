c4459170cfd0c8f5d489d570cf08bfc454e75824
Łukasz <lukasz13102001@o2.pl>
2023-03-27 16:50:31 +0200

    initial commit

---
+++ backend/filmhouse/src/main/java/com/example/filmhouse/FilmhouseApplication.java
@@ -0,0 +1,13 @@
+package com.example.filmhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmhouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmhouseApplication.class, args);
+	}
+
+}
